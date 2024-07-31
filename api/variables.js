6df214b4e71dff0c957ecc38de3409aa5d7e9d29
shaik-moazzam <shaikmoazzam1@gmail.com>
2024-07-31 17:27:27 +0530

    puahh

--- api/variables.js
+++ api/variables.js
@@ -1,8 +1,8 @@
 // export const BASE_URL = "http://localhost:3003/api/v1/";
 // export const BASE_URL = "https://holysprig-backend.onrender.com/api/v1/";
-export const BASE_URL =
-  "https://27f2-2409-40f0-1003-2bf0-bce2-53bd-101a-a49.ngrok-free.app/api/v1/";
-// export const BASE_URL = "https://backend.holyspring.in/api/v1/";
+// export const BASE_URL =
+//   "https://27f2-2409-40f0-1003-2bf0-bce2-53bd-101a-a49.ngrok-free.app/api/v1/";
+export const BASE_URL = "https://backend.holyspring.in/api/v1/";
 export const SOCKET_BASE_URL = "https://backend.holyspring.in";
 export const IMAGE_BASE_URL = "https://backend.holyspring.in/";
 // export const BASE_URL = "https://backend.zainii.com:8443/api/v1/";
