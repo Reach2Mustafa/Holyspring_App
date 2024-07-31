7a685aa690be616cb85c13d953afb9a4ec7c897e
reach2mohammed <mustafashareef@devsed.com>
2024-07-31 17:40:03 +0530

    done

--- api/variables.js
+++ api/variables.js
@@ -1,7 +1,6 @@
 // export const BASE_URL = "http://localhost:3003/api/v1/";
 // export const BASE_URL = "https://holysprig-backend.onrender.com/api/v1/";
-// export const BASE_URL =
-//   "https://27f2-2409-40f0-1003-2bf0-bce2-53bd-101a-a49.ngrok-free.app/api/v1/";
+// export const BASE_URL = "https://8168-2409-40f0-1017-92ee-bda6-f25c-b560-5f4b.ngrok-free.app/api/v1/";
 export const BASE_URL = "https://backend.holyspring.in/api/v1/";
 export const SOCKET_BASE_URL = "https://backend.holyspring.in";
 export const IMAGE_BASE_URL = "https://backend.holyspring.in/";
